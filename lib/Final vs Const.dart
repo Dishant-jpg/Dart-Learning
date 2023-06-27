cd74092915ced30af68696d31206d3a6ef618331
Dishant Gupta <84134946+Dishant-jpg@users.noreply.github.com>
2023-06-27 20:06:32 +0530

    first commit

---
+++ lib/Final vs Const.dart
@@ -0,0 +1,20 @@
+void main() {
+  print('Welcome To Dart');
+
+  //For Final
+  //final String name;
+  //name = 'Dishant';
+
+  //OR
+
+  //final dynamic name;
+  //name = 'Dishant';
+
+  //For CONST
+  // ignore: unused_local_variable
+  const names = ["Dishant", "Vishal", "Chirag", "Hardik"];
+
+  names.add("Peter");
+
+  // names = ["Peter", "Raju", "Ravi"];
+}
